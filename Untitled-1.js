66c4620c8a99f0a13e04d581ad76d00294f9afb1
dnshkumar2711 <126949579+dnshkumar2711@users.noreply.github.com>
2026-06-22 22:35:34 +0530

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,30 +1,3 @@
-<script>
-const data = {
-  "Cloud": "AWS, Azure, GCP - Cloud infrastructure design & deployment",
-  "DevOps": "CI/CD, Jenkins, Git, Terraform automation pipelines",
-  "Containers": "Docker & Kubernetes microservices management",
-  "Linux": "RHEL, Ubuntu server administration & security"
-};
-
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-
-    let title = card.querySelector("h3").innerText;
-
-    document.getElementById("modalTitle").innerText = title;
-    document.getElementById("modalText").innerText = data[title];
-
-    document.getElementById("skillModal").style.display = "flex";
-  });
-});
-
-function closeModal(){
-  document.getElementById("skillModal").style.display = "none";
+function downloadResume(){
+alert("Resume Download Started 🚀");
 }
-
-window.onclick = function(e){
-  if(e.target.id === "skillModal"){
-    closeModal();
-  }
-}
-</script>
